75bc252a064af6bfb212603d0586da17e83530af
Prajjwal Kapoor <prajjwal.kapoor.mail@gmail.com>
2023-01-29 13:13:23 +0530

    job details

--- frontend/src/components/EmployeeCard.jsx
+++ frontend/src/components/EmployeeCard.jsx
@@ -1,39 +1,72 @@
 import React from "react";
 
-const EmployeeCard = ({name,email,skills,location}) => {
-    return (
-        <div className="flex flex-col items-center sm:items-start mb-4">
-            <div className="dark:bg-gray-800 max-w-fit flex flex-col justify-start sm:space-x-24 items-start sm:flex-row gap-2 sm:items-center px-5 py-4 rounded-lg shadow-lg">
-                <div className="flex flex-row space-x-8 sm:flex-col sm:space-x-0">
-                    <div className="text-md leading-5 font-medium text-gray-900 dark:text-white">
-                        {name}
-                    </div>
-                    <div className="text-sm leading-5 text-gray-500">{email}</div>
-                </div>
-                <div className="flex flex-row space-x-8 sm:flex-col sm:space-x-0">
-                    <div className="text-md leading-5 text-gray-900 dark:text-white">Software Engineer</div>
-                    <div className="text-sm leading-5 text-gray-500">Web dev</div>
-                </div>
-                <div className="flex flex-col">
-                    <div>
-                        <span className="sm:px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">Active</span>
-                    </div>
-                </div>
-                <div className="flex flex-col">
-                    <div className="sm:px-6 sm:py-4 whitespace-no-wrap text-sm leading-5 text-gray-500 dark:text-white">
-                        Owner
-                    </div>
-                </div>
-                <div className="flex flex-col">
-                    <div className="sm:px-6 sm:py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium">
-                        <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                            Marked as Interviewee
-                        </a>
-                    </div>
-                </div>
-            </div>
+const EmployeeCard = ({ candidate }) => {
+  const { name, email, degree, interview, skills } = candidate;
+  return (
+    <div className='flex flex-col items-center sm:items-start mb-4 '>
+      <div className='dark:bg-gray-800 min-w-full md:grid grid-cols-11  justify-between sm:space-x-24 items-start sm:flex-row gap-2 sm:items-center px-5 py-4 rounded-lg shadow-lg'>
+        <div className='flex flex-row space-x-8 sm:flex-col sm:space-x-0'>
+          <div className='text-md leading-5 font-medium text-gray-900 dark:text-white'>
+            {name}
+          </div>
+          <div className='text-sm leading-5 text-gray-500'>
+            {/* linkedin icon */}
+            <a href={email} className='flex gap-1 mt-2' target='_blank'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='24'
+                height='24'
+                viewBox='0 0 24 24'
+                fill='rgb(107 114 128)'
+                className='w-4 h-4'
+              >
+                <path d='M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z' />
+              </svg>
+              Mail
+            </a>
+          </div>
         </div>
-    )
+        <div className='flex flex-row space-x-8 sm:flex-col sm:space-x-0 col-span-4'>
+          <div className='text-md leading-5 text-gray-900 dark:text-white'>
+            {degree.split("-")[0]}
+          </div>
+          <div className='text-sm leading-5 text-gray-500'>
+            {skills.split(" ").slice(0, 6).join(" ")}
+          </div>
+        </div>
+        <div className='flex flex-col col-span-2'>
+          <div>
+            <span
+              className={`sm:px-2 inline-flex text-xs leading-5 min-w-full font-semibold rounded-full ${
+                interview
+                  ? "bg-green-100 text-green-800"
+                  : "bg-red-100 text-red-800"
+              } `}
+            >
+              {interview ? "Interviewing" : "Not Interviewed"}
+            </span>
+          </div>
+        </div>
+        <div className='flex flex-col'>
+          <div className='sm:px-6 sm:py-4 whitespace-no-wrap text-sm leading-5 text-gray-500 dark:text-white'>
+            Owner
+          </div>
+        </div>
+        <div className='flex flex-col col-span-3'>
+          <div className='sm:px-6 sm:py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium'>
+            <a
+              onClick={() => {}}
+              className={`text-indigo-600 hover:text-indigo-900 ${
+                interview && "opacity-50"
+              }`}
+            >
+              {interview ? "Interviewing" : "Mark as Interviewee"}
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default EmployeeCard;
